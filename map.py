e7c4af6150d7c13973778a8f210f77d30dc04403
Raza <rsc1099@gmail.com>
2026-01-26 09:01:38 +0000

    map format

--- map.py
+++ map.py
@@ -11,19 +11,53 @@ from streamlit_folium import st_folium, folium_static
 req = requests.get('https://spotting-api.onrender.com/spottings')
 data = req.json()
 df=pd.DataFrame(data)
+adf=pd.read_csv('spotted-animals.csv')
 df['latitude']=df['location'].apply(lambda r: float(r.split(',')[0]))
 df['longitude']=df['location'].apply(lambda r: float(r.split(',')[1]))
+df['date_time']=pd.to_datetime(df['date_time'])
 df=df.drop('location', axis=1)
+adf=adf.rename(columns={"name":"animal_name"})
+adf=adf[['animal_name','species','capture_points']]
+fdf=pd.merge(df,adf,on='animal_name',how='left')
+fdf['rarity']=fdf['capture_points'].apply(lambda x:"Common" if x < 20 else "Rare" if x < 40 else "Exceptional")
 
-m = folium.Map(location=[df.latitude.mean(), df.longitude.mean()],
-               zoom_start=3, control_scale=True)
+st.write(fdf)
 
-for i,row in df.iterrows():
-    iframe = folium.IFrame(f"""Spotting:Animal: {str(row["animal_name"])}
+m = folium.Map(location=[54.546687,-3.881687],
+               zoom_start=5.5, control_scale=True)
 
-""")
+
+for i,row in fdf.iterrows():
+    lines = ["Spottings:", f"Animal: {str(row['animal_name'])}",f"Rarity: {row['rarity']}",f"Count: {str(row['animal_count'])}", f"When: {row['date_time']}" ,f"Username: {str(row['username'])}",f"<br><img src='{str(row['image_url'])}' style='max-height:50px;'>" ]
+    html_content = "<br>".join(lines)
+    iframe = folium.IFrame(html=html_content)
     popup = folium.Popup(iframe, min_width=300, max_width=300)
-    folium.Marker(location=[row['latitude'],row['longitude']],
-                  popup = popup, c=row['animal_name']).add_to(m)
-    
-st_data = folium_static(m, width=700)
+    if row['rarity'] == 'Common':
+        folium.Marker(
+            location=[row['latitude'],row['longitude']],
+            popup=popup, 
+            icon=folium.Icon(color='green',icon='glyphicon glyphicon-pushpin'),
+            c=row['animal_name']
+        ).add_to(m)
+    elif row['rarity'] == 'Rare':
+        folium.Circle(
+            location=[row['latitude'],row['longitude']],
+            radius=100,
+            stroke=False,
+            fill=True,
+            fill_opacity=0.6,
+            opacity=1,
+            popup=popup
+        ).add_to(m)
+
+    else:
+        folium.Circle(
+            location=[row['latitude'],row['longitude']],
+            radius=100,
+            stroke=False,
+            fill=True,
+            fill_opacity=0.6,
+            opacity=1,
+            popup=popup
+        ).add_to(m)
+st_data = st_folium(m, width=700,returned_objects=[])
